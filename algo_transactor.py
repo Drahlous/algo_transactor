ac594e83b8952bbc1553ca6fbc82580c528204a2
mstachow <matttstachowiak@gmail.com>
2021-04-03 17:12:20 -0600

    Update spacing

--- algo_transactor.py
+++ algo_transactor.py
@@ -21,22 +21,22 @@ print(json.dumps(status, indent=4))
 
 # Check the suggested transaction params
 try:
-    params = algod_client.suggested_params()
-    print(json.dumps(vars(params), indent=4))
+  params = algod_client.suggested_params()
+  print(json.dumps(vars(params), indent=4))
 except Exception as e:
-    print(e)
+  print(e)
 
 
 
-# ==============    
-# Generate Wallet   
-# ==============    
+# ==============
+# Generate Wallet
+# ==============
 
 # Helper function, generate an address and private key
 def generate_algorand_keypair():
-    private_key, address = account.generate_account()
-    print("Address: {}".format(address))
-    print("Passphrase: {}".format(mnemonic.from_private_key(private_key)))
+  private_key, address = account.generate_account()
+  print("Address: {}".format(address))
+  print("Passphrase: {}".format(mnemonic.from_private_key(private_key)))
 
 # 25-word mnemonic passphrase
 passphrase = "box spread enforce suspect unit immense stove exact palace window effort wide urge need switch leisure write lab drum mistake penalty they uniform above immense"
@@ -53,9 +53,9 @@ print("Account Balance: {} micro_algos".format(account_info.get('amount')))
 
 
 
-# ==============    
-# Constructing Transactions  
-# ==============  
+# ==============
+# Constructing Transactions
+# ==============
 
 # Create a wallet to receive funds
 receiving_passphrase = "actor purpose funny fetch shadow fox heavy police side snack silent purse illness average whip marble rookie useless mouse dutch involve stick penalty absent step"
@@ -87,54 +87,48 @@ txid = algod_client.send_transaction(signed_txn)
 print("Successfully sent transaction with txID: {}".format(txid))
 
 
-# ==============    
+# ==============
 # ! Always wait for the transaction to be confirmed by the network
-# ! Transaction will be confirmed within 1 block.
-# ! Read back the transaction before proceeding   
-# ==============  
+# ! Transaction will be confirmed within 1 block
+# ! Read back the transaction before proceeding
+# ==============
 
 # utility for waiting on a transaction confirmation
 def wait_for_confirmation(client, transaction_id, timeout):
-    """
-    Wait until the transaction is confirmed or rejected, or until 'timeout'
-    number of rounds have passed.
-    Args:
-        transaction_id (str): the transaction to wait for
-        timeout (int): maximum number of rounds to wait
-    Returns:
-        dict: pending transaction information, or throws an error if the transaction
-            is not confirmed or rejected in the next timeout rounds
-    """
-    start_round = client.status()["last-round"] + 1;
-    current_round = start_round
+  """
+  Wait until the transaction is confirmed or rejected, or until 'timeout'
+  number of rounds have passed.
+  Args:
+    transaction_id (str): the transaction to wait for
+    timeout (int): maximum number of rounds to wait
+  Returns:
+    dict: pending transaction information, or throws an error if the transaction
+      is not confirmed or rejected in the next timeout rounds
+  """
+  start_round = client.status()["last-round"] + 1
+  current_round = start_round
 
-    while current_round < start_round + timeout:
-        try:
-            pending_txn = client.pending_transaction_info(transaction_id)
-        except Exception:
-            return 
-        if pending_txn.get("confirmed-round", 0) > 0:
-            return pending_txn
-        elif pending_txn["pool-error"]:  
-            raise Exception(
-                'pool error: {}'.format(pending_txn["pool-error"]))
-        client.status_after_block(current_round)                   
-        current_round += 1
-    raise Exception(
-        'pending tx not found in timeout rounds, timeout value = : {}'.format(timeout))
+  while current_round < start_round + timeout:
+    try:
+      pending_txn = client.pending_transaction_info(transaction_id)
+    except Exception:
+      return
+    if pending_txn.get("confirmed-round", 0) > 0:
+      return pending_txn
+    elif pending_txn["pool-error"]:
+      raise Exception('pool error: {}'.format(pending_txn["pool-error"]))
+    client.status_after_block(current_round)
+    current_round += 1
+  raise Exception('pending tx not found in timeout rounds, timeout value = : {}'.format(timeout))
  
-# wait for confirmation 
+# wait for confirmation
 try:
-    confirmed_txn = wait_for_confirmation(algod_client, txid, 4)  
+  confirmed_txn = wait_for_confirmation(algod_client, txid, 4)
 except Exception as err:
-    print(err)
+  print(err)
 
-print("Transaction information: {}".format(
-    json.dumps(confirmed_txn, indent=4)))
-print("Decoded note: {}".format(base64.b64decode(
-    confirmed_txn["txn"]["txn"]["note"]).decode()))
-
-
+print("Transaction information: {}".format(json.dumps(confirmed_txn, indent=4)))
+print("Decoded note: {}".format(base64.b64decode(confirmed_txn["txn"]["txn"]["note"]).decode()))
 
 
 quit()
